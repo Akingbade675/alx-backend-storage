bd25735bad9503766dbd83afa582301f462b08d8
Akingbade675 <akingbadebasit26@gmail.com>
2023-05-07 00:32:27 +0000

    added 5-valid_email.sql file.

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,3 +1,11 @@
 -- creates a trigger
 -- resets the attribute valid_email only when the email has been changed.
-delimeter //
+DELIMITER //
+CREATE TRIGGER `reset_email` BEFORE UPDATE ON `users`
+FOR EACH ROW
+BEGIN
+  IF NEW.email != OLD.email THEN
+    SET NEW.valid_email = 0;
+  END IF;
+END//
+DELIMITER ;

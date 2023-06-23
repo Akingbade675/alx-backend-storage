ca185db203ee5bf4ee4b9d3788dbc188a5f1c0c5
Akingbade675 <akingbadebasit26@gmail.com>
2023-06-23 14:48:55 +0000

    My implementation of the redis project

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,44 +1,21 @@
 #!/usr/bin/env python3
-"""0. Create a Cache class. In the __init__ method, store an instance of
-the Redis client as a private variable named _redis (using redis.Redis())
-and flush the instance using flushdb.
-
-Create a store method that takes a data argument and returns a string.
-The method should generate a random key (e.g. using uuid), store the
-input data in Redis using the random key and return the key.
-
-Type-annotate store correctly. Remember that data can be a str,
-bytes, int or float.
-
-
-1. Redis only allows to store string, bytes and numbers (and lists thereof).
-Whatever you store as single elements, it will be returned as a byte string.
-Hence if you store "a" as a UTF-8 string, it will be returned as b"a" when
-retrieved from the server.
-
-In this exercise we will create a get method that take a key string argument
-and an optional Callable argument named fn. This callable will be used to
-convert the data back to the desired format.
-
-Remember to conserve the original Redis.get behavior if the key does not exist.
-Also, implement 2 new methods: get_str and get_int that will automatically
-parametrize Cache.get with the correct conversion function.
-"""
+'''Redis with Python'''
 
 
 import redis
-from uuid import uuid4
-from typing import Union, Callable, Optional
+import uuid
+from typing import Union, Optional, Callable
 from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
-    """returns a Callable"""
+    '''count how many times methods
+    of the Cache class are called'''
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """wrapper for decorated function"""
+        '''the wrapper definition'''
         self._redis.incr(key)
         return method(self, *args, **kwargs)
 
@@ -46,85 +23,71 @@ def count_calls(method: Callable) -> Callable:
 
 
 def call_history(method: Callable) -> Callable:
-    """store the history of inputs and outputs"""
+    '''decorator to store the history of inputs
+    and outputs for a particular function'''
+    name = method.__qualname__
+
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """wrapper for the decorated function"""
-        input = str(args)
-        self._redis.rpush(method.__qualname__ + ":inputs", input)
-        output = str(method(self, *args, **kwargs))
-        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        '''wrapper function'''
+        self._redis.rpush('{}:inputs'.format(name), str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush('{}:outputs'.format(name), output)
         return output
 
     return wrapper
 
 
 def replay(fn: Callable):
-    """display the history of calls of a particular function"""
+    '''display the history of calls
+    of a particular function'''
+    name = fn.__qualname__
     r = redis.Redis()
-    function_name = fn.__qualname__
-    value = r.get(function_name)
-    try:
-        value = int(value.decode("utf-8"))
-    except Exception:
-        value = 0
+    func_call_count = r.get(name).decode('utf-8')
 
-    # print(f"{function_name} was called {value} times")
-    print("{} was called {} times:".format(function_name, value))
-    # inputs = r.lrange(f"{function_name}:inputs", 0, -1)
-    inputs = r.lrange("{}:inputs".format(function_name), 0, -1)
+    print('{} was called {} times:'.format(name, func_call_count))
 
-    # outputs = r.lrange(f"{function_name}:outputs", 0, -1)
-    outputs = r.lrange("{}:outputs".format(function_name), 0, -1)
+    inputs = r.lrange('{}:inputs'.format(name), 0, -1)
+    outputs = r.lrange('{}:outputs'.format(name), 0, -1)
+
+    # decode binary to string format
+    def decode(value): return value.decode('utf-8')
 
     for input, output in zip(inputs, outputs):
-        try:
-            input = input.decode("utf-8")
-        except Exception:
-            input = ""
-
-        try:
-            output = output.decode("utf-8")
-        except Exception:
-            output = ""
-
-        # print(f"{function_name}(*{input}) -> {output}")
-        print("{}(*{}) -> {}".format(function_name, input, output))
+        print("{}(*{}) -> {}".format(name, decode(input), decode(output)))
 
 
-class Cache:
-    """Create a Cache class"""
+class Cache():
+    '''Cache class'''
 
     def __init__(self):
-        """store an instance of the Redis client"""
+        '''store an instance of the Redis client'''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """generate a random key"""
-        random_key = str(uuid4())
-        self._redis.set(random_key, data)
-        return random_key
+        '''generate a random key'''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
 
     def get(self, key: str,
-            fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
-        """convert the data back to the desired format"""
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''convert data back to the desired format'''
         value = self._redis.get(key)
         if fn:
             value = fn(value)
         return value
 
-    def get_str(self, key: str) -> str:
-        """automatically parametrize Cache.get with the correct
-        conversion function"""
+    def get_str(self, key) -> str:
+        '''get string'''
         value = self._redis.get(key)
         return value.decode("utf-8")
 
-    def get_int(self, key: str) -> int:
-        """automatically parametrize Cache.get with the correct
-        conversion function"""
+    def get_int(self, key) -> int:
+        '''get int'''
         value = self._redis.get(key)
         try:
             value = int(value.decode("utf-8"))
--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,54 +1,44 @@
 #!/usr/bin/env python3
-"""In this tasks, we will implement a get_page function
-(prototype: def get_page(url: str) -> str:). The core of
-the function is very simple. It uses the requests module
-to obtain the HTML content of a particular URL and returns it.
-
-Start in a new file named web.py and do not reuse the code
-written in exercise.py.
-
-Inside get_page track how many times a particular URL was
-accessed in the key "count:{url}" and cache the result with
-an expiration time of 10 seconds.
-
-Tip: Use http://slowwly.robertomurray.co.uk to simulate
-a slow response and test your caching."""
+'''Redis cache'''
 
 
-import redis
 import requests
+import redis
+from typing import Callable
 from functools import wraps
 
-r = redis.Redis()
 
+def call_count(fn: Callable) -> Callable:
+    '''track how many times a
+    particular URL was accessed
+    in the key "count:{url}" and
+    cache the result with an
+    expiration time of 10 seconds'''
 
-def url_access_count(method):
-    """decorator for get_page function"""
-    @wraps(method)
+    @wraps(fn)
     def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = r.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
+        '''wrapper function'''
+        r = redis.Redis()
 
-            # Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
+        cache_key = 'cache:{}'.format(url)
+        cache_value = r.get(cache_key)
+        if cache_value:
+            return cache_value.decode('utf-8')
 
-        r.incr(key_count)
-        r.set(key, html_content, ex=10)
-        r.expire(key, 10)
+        # get_page if url is no cache or expired
+        key = 'count:{}'.format(url)
+        html_content = fn(url)
+
+        r.incr(key)
+        r.set(cache_key, html_content)
+        r.expire(cache_key, 10)
         return html_content
+
     return wrapper
 
 
-@url_access_count
+@call_count
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
-    results = requests.get(url)
-    return results.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    '''returns the HTML content of a particular URL'''
+    result = requests.get(url)
+    return result.text
